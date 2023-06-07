a730efe5bcc4be074726ed934a98f52827d3e6aa
Sam <sam.a863.nz@gmail.com>
2023-06-08 11:17:23 +1200

    Test class.py modified

--- Test class.py
+++ Test class.py
@@ -1,8 +1,12 @@
 class sam_class:
-    def __init__(self,name,age):
+    def __init__(self, name,age):
         self.name = name
         self.age = age
 
-A = sam_class("Samarth Agarwal", 12)
-print(A.name)
+    def sam(self):
+        print("BHS is a good school")
 
+MH370 = sam_class("Malaysian Airlines Flight 370", 12)
+print(MH370.name)
+print(MH370.age)
+print(MH370.sam())

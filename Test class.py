db87d3e1064f6c917ee954868d5eb2960d9edb49
Sam <sam.a863.nz@gmail.com>
2023-06-08 11:14:20 +1200

    Test class.py file readded

---
+++ Test class.py
@@ -0,0 +1,11 @@
+class sam_class:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+    def sam(self):
+        print("BHS is a good school")
+
+MH370 = sam_class("Malaysian Airlines Flight 370", 12)
+print(MH370.name)
+print(MH370.sam())

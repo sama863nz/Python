fbe7b8beacdcb1a538f6fb8f906bbcf6611d8166
Sam <sam.a863.nz@gmail.com>
2023-08-06 12:30:45 +1200

    new file added

---
+++ new_class.py
@@ -0,0 +1,11 @@
+class sam():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+prime = sam("Malaysian 370", 9)
+print(prime)
+
+class new(sam):
+    pass
+Malaysia = new("Malaysian 370, contact Ho Chi Minh 120.9, Good Night.", " Good Night Malaysian 370.")
+print(Malaysia)

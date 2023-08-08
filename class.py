abfe0e29e7508d7c72a844ec3cbb041bccf6ed1d
Sam <sam.a863.nz@gmail.com>
2023-08-08 16:36:43 +1200

    files modified and added

--- class.py
+++ class.py
@@ -1,12 +1,10 @@
-class sam_class:
-    def __init__(self,age,name):
+class sam():
+    def __init__(self, name, age):
         self.age = age
         self.name = name
-
-class sam_new(sam_class):
+Heaton = sam("Hello, I am Samarth and I am ", "12 years old!")
+print(Heaton.name + Heaton.age)
+class new(sam):
     pass
-MAS = sam_class(53, "Captain Zaharie Ahemad Shah")
-MAS1 = sam_new(27, "First Officer Fariq Hammid")
-print(MAS.name)
-print(MAS1.name)
-
+monkey = new("Malaysian 370, Contact Ho Chi Minh 120.9, Good Night.", ", Malaysian 370.")
+print(monkey.name)
--- new_class.py
+++ new_class.py
@@ -7,7 +7,9 @@ prime = sam("my name is Samarth and my age is ", "12")
 print(prime.name + prime.age)
 
 class new(sam):
-    pass
-    
+   pass
+
 Car = new("Malaysian 370", 12)
 print(Car.name)
+
+

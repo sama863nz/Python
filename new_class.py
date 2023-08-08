e79cc8c7e0d688280e56d4342f9072667819d93e
Sam <sam.a863.nz@gmail.com>
2023-08-08 16:19:31 +1200

    file modified

--- new_class.py
+++ new_class.py
@@ -2,7 +2,8 @@ class sam():
     def __init__(self, name, age):
         self.name = name
         self.age = age
-prime = sam(my name is Samarth and my age is ", "12")
+
+prime = sam("my name is Samarth and my age is ", "12")
 print(prime.name + prime.age)
 
 class new(sam):

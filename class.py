555f398563cdf8b8e59fd5b82e8ee4c9a0cc7e82
Sam <sam.a863.nz@gmail.com>
2023-10-04 16:30:53 +1300

    file modified

--- class.py
+++ class.py
@@ -1,10 +1,10 @@
 class sam():
-    def __init__(self, name, age):
-        self.age = age
-        self.name = name
-Heaton = sam("Hello, I am Samarth and I am ", "12 years old!")
-print(Heaton.name + Heaton.age)
-class new(sam):
-    pass
-monkey = new("Malaysian 370, Contact Ho Chi Minh 120.9, Good Night.", ", Malaysian 370.")
-print(monkey.name)
+    def __init__(self):
+        pass
+    
+    def CI(self, P, R, T):
+        A = P*(1 + R/100)**T
+        return A
+    
+prime = sam()
+print(prime.CI(55555,96,6))
